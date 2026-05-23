270ad96d82ec694f2e47b56c3005600786af0116
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2026-05-23 20:26:19 +0700

    feat(ZaloOrder): add belongsTo customer relationship

--- app/Models/ZaloOrder.php
+++ app/Models/ZaloOrder.php
@@ -36,6 +36,11 @@ class ZaloOrder extends Model
     public $incrementing = true;
     protected $keyType = 'int';
 
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
+
     public function items()
     {
         return $this->hasMany(ZaloOrderItem::class, 'order_id');
