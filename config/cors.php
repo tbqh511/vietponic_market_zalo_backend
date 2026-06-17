f280c694696d10bd1f1ef66fc4140f7d24ed6eb0
Thái Bình Quang Huy <tbqh0511@yahoo.com.vn>
2026-06-17 11:32:56 +0700

    feat(cors): add PATCH to CORS allowed methods

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
     'allowed_origins' => ['*'],
 
